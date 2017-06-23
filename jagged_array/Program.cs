0d21dc0ba3fc47923183e0cf173fbd42b33a991a
eemmhha <eemmhha@live.com>
2017-06-23 17:26:09 +0200

    check

--- jagged_array/Program.cs
+++ jagged_array/Program.cs
@@ -10,6 +10,20 @@ namespace jagged_array
     {
         static void Main(string[] args)
         {
+            int[][] arr = new int[3][];
+            arr[0] =new int[]{0,1,2,3,4 };
+            arr[1] = new int[] { 5,6,7,8 };
+            arr[2] = new int[] { 9,5,1};
+            for (int i=0;i<arr.Length;i++)
+            {
+                Console.Write("my array index ({0})",i);
+                for(int j =0;j<arr[i].Length;j++)
+                {
+                    Console.Write("{0}", arr[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
